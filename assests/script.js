a6aff93148089a8113d7f9e2560ededafe67297c
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-02 17:29:19 -0500

    added file strucutre updated html and css

---
+++ assests/script.js
@@ -0,0 +1,27 @@
+
+
+const intro = document.querySelector(".intro");
+const start = document.querySelector(".start-bt");
+const quiz = document.querySelector(".quiz");
+const time = document.querySelector(".seconds")
+let secondsLeft = 10;
+start.addEventListener("click", function () {
+    console.log("button was clicked")
+    quiz.classList.remove("hide");
+    intro.classList.add("hide");
+    setTime()
+});
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      time.textContent = secondsLeft 
+  
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+       
+      }
+  
+    }, 1000);
+  }
