d316f1744d763c3d6026442c7e5c829e8aa65274
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-04 09:29:40 -0500

    git@github.com:DDesta25/work-schedule-planner.git

--- assests/question.js
+++ assests/question.js
@@ -46,37 +46,37 @@ var question = [
 
 
 // title: "What is the tallest land animal",
-//choices: [
-    //"chipmunks", "giraffes", "lions",
-//],
-//answers: "giraffe"
-//},
+// choices: [
+//     "chipmunks", "giraffes", "lions",
+// ],
+// answers: "giraffe"
+// },
 
 // {
-//title: "which animal has the strongest bite force",
-//choices: [
-    //"crocodiles", "donkeys", "cats",
-//],
-//answers: "crocodiles"
-//},
-//{
-//title: "which animal has the longest life expectancy",
-//choices: [
-  //  "dogs", "alligators", "immortal jellyfish",
-//],
-//answers: "immortal jellyfish"
-//},             
-//{
-//title: "what is the strongest animal in the planet",
-//choices: [
- //   "pandas", "blue whales", "tiger",
-//],
-//answers: "blue whales"
-//},
-//{
-//title: "what is the cutest animal on the planet",
-//choices: [
-    //"pandas", "chimpanzees", "cobra",
-//],
-//answers: "pandas"
-//},
+// title: "which animal has the strongest bite force",
+// choices: [
+//     "crocodiles", "donkeys", "cats",
+// ],
+// answers: "crocodiles"
+// },
+// {
+// title: "which animal has the longest life expectancy",
+// choices: [
+//    "dogs", "alligators", "immortal jellyfish",
+// ],
+// answers: "immortal jellyfish"
+// },             
+// {
+// title: "what is the strongest animal in the planet",
+// choices: [
+//    "pandas", "blue whales", "tiger",
+// ],
+// answers: "blue whales"
+// },
+// {
+// title: "what is the cutest animal on the planet",
+// choices: [
+//     "pandas", "chimpanzees", "cobra",
+// ],
+// answers: "pandas"
+// },
