27a982d0c212a7451b901547c06c750cf5715ca0
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-04 14:56:54 -0500

    fixed the question title not rendering

--- assests/question.js
+++ assests/question.js
@@ -1,50 +1,33 @@
 var question = [
-    {
-        title: "testtitle1",
-        choices: [
-            "choice1", "choice2", "choice3",
-        ],
-        answers: "choice1"
-    },
+  {
+    title: "What is the tallest land animal",
+    choices: ["chipmunks", "giraffes", "lions"],
+    answers: "giraffe",
+  },
 
-    {
-        title: "testtitle2",
-        choices: [
-            "choice1", "choice2", "choice3",
-        ],
-        answers: "choice1"
-    },
-    {
-        title: "testtitle3",
-        choices: [
-            "choice1", "choice2", "choice3",
-        ],
-        answers: "choice1"
-    },             
-    {
-        title: "testtitle4",
-        choices: [
-            "choice1", "choice2", "choice3",
-        ],
-        answers: "choice1"
-    },
-    {
-        title: "testtitle5",
-        choices: [
-            "choice1", "choice2", "choice3",
-        ],
-        answers: "choice1"
-    },
-    {
-        title: "testtitle6",
-        choices: [
-            "choice1", "choice2", "choice3",
-        ],
-        answers: "choice1"
-    },
-]
+  {
+    title: "which animal has the strongest bite force",
+    choices: ["crocodiles", "donkeys", "cats"],
+    answers: "crocodiles",
+  },
+  {
+    title: "which animal has the longest life expectancy",
+    choices: ["dogs", "alligators", "immortal jellyfish"],
+    answers: "immortal jellyfish",
+  },
+  {
+    title: "what is the strongest animal in the planet",
+    choices: ["pandas", "blue whales", "tiger"],
+    answers: "blue whales",
+  },
+  {
+    title: "what is the cutest animal on the planet",
+    choices: ["pandas", "chimpanzees", "cobra"],
+    answers: "pandas",
+  },
+];
 
-
+// {
 // title: "What is the tallest land animal",
 // choices: [
 //     "chipmunks", "giraffes", "lions",
@@ -65,7 +48,7 @@ var question = [
 //    "dogs", "alligators", "immortal jellyfish",
 // ],
 // answers: "immortal jellyfish"
-// },             
+// },
 // {
 // title: "what is the strongest animal in the planet",
 // choices: [
--- assests/script.js
+++ assests/script.js
@@ -1,51 +1,47 @@
-
-var questionIndex=0
-console.log(question) 
+var questionIndex = 0;
+console.log(question);
 const intro = document.querySelector(".intro");
 const start = document.querySelector(".start-bt");
 const quiz = document.querySelector(".quiz");
-const time = document.querySelector(".seconds")
-const questionCurrent = document.querySelector(".question")
-const answer = document.querySelector(".answer-btn")
+const time = document.querySelector(".seconds");
+const questionTitleEL = document.querySelector(".question");
+const answer = document.querySelector(".answer-btn");
 let secondsLeft = 10;
 start.addEventListener("click", function () {
-    console.log("button was clicked")
-    quiz.classList.remove("hide");
-    intro.classList.add("hide");
-    setTime()
+  console.log("button was clicked");
+  quiz.classList.remove("hide");
+  intro.classList.add("hide");
+  setTime();
 });
 function setTime() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      time.textContent = secondsLeft 
-  
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-       
-      }
-  
-    }, 1000);
+  // Sets interval in variable
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    time.textContent = secondsLeft;
 
-  }
+    if (secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
 function getQuestion() {
-  var currentQuestion = question[questionIndex]
-  console.log(currentQuestion)
-  var questionTitle = document.getElementsByClassName("question")
-  questionTitle.textContent = currentQuestion.title
-  answer.innerHTML = "" 
+  var currentQuestion = question[questionIndex];
+  console.log(currentQuestion);
+
+  questionTitleEL.textContent = currentQuestion.title;
+  answer.innerHTML = "";
   for (let i = 0; i < currentQuestion.choices.length; i++) {
-   var singleChoice= currentQuestion.choices[i]
-   console.log(singleChoice)
-   var choiceElement = document.createElement("button");
-   choiceElement.setAttribute("value",singleChoice)
-   choiceElement.textContent = i+1+singleChoice
-   answer.appendChild(choiceElement)
+    var singleChoice = currentQuestion.choices[i];
+    console.log(singleChoice);
+    var choiceElement = document.createElement("button");
+    choiceElement.setAttribute("value", singleChoice);
+    choiceElement.textContent = i + 1 + singleChoice;
+    answer.appendChild(choiceElement);
   }
 }
-getQuestion()
+getQuestion();
 // create function when you select a question
 // create function when quiz ends, remember to clear timer
 //  create a function when saving high scores
-// create an event listener when you click the choice and also submitting high score 
+// create an event listener when you click the choice and also submitting high score
