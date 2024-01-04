ab57f2329b739a053ff6efecdd6ecba71365710f
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-04 16:13:21 -0500

    added feedback and answer logic. corrent bug = answer on click none function

--- assests/script.js
+++ assests/script.js
@@ -1,3 +1,4 @@
+var feedback = document.querySelector("#feedback");
 var questionIndex = 0;
 console.log(question);
 const intro = document.querySelector(".intro");
@@ -6,12 +7,14 @@ const quiz = document.querySelector(".quiz");
 const time = document.querySelector(".seconds");
 const questionTitleEL = document.querySelector(".question");
 const answer = document.querySelector(".answer-btn");
+console.log(feedback);
 let secondsLeft = 10;
 start.addEventListener("click", function () {
   console.log("button was clicked");
   quiz.classList.remove("hide");
   intro.classList.add("hide");
   setTime();
+  getQuestion();
 });
 function setTime() {
   // Sets interval in variable
@@ -36,12 +39,33 @@ function getQuestion() {
     console.log(singleChoice);
     var choiceElement = document.createElement("button");
     choiceElement.setAttribute("value", singleChoice);
-    choiceElement.textContent = i + 1 + singleChoice;
+    choiceElement.textContent = singleChoice;
     answer.appendChild(choiceElement);
   }
 }
-getQuestion();
+function answerClick(event) {
+  var buttonClick = event.target;
+  if (!buttonClick.matches(".answer")) {
+    return;
+  }
+  if (buttonClick.value !== question[questionIndex].answers) {
+    console.log("value" + buttonClick.value);
+    console.log("answer" + question[questionIndex].answers);
+    secondsLeft -= 15;
+    time.textContent = secondsLeft;
+    feedback.textContent = "oops";
+  } else {
+    feedback.textContent = "right on";
+  }
+  setTimeout(function () {
+    feedback.setAttribute("class", "feedback hide");
+  }, 1000);
+  questionIndex++;
+}
+
 // create function when you select a question
 // create function when quiz ends, remember to clear timer
 //  create a function when saving high scores
 // create an event listener when you click the choice and also submitting high score
+
+answer.addEventListener("click", console.log("i did it"));
